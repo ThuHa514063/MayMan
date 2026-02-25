1f58099c4552aa08445f196beef732176f06c776
Thu Ha <phanthithuha2018.06@gmail.com>
2026-02-25 23:08:20 +0700

    Update w.py

--- w.py
+++ w.py
@@ -41,7 +41,6 @@ st.markdown("""
         margin: 20px auto; overflow: hidden; box-shadow: 0 20px 50px rgba(0,0,0,0.8);
     }
     
-    /* FIX CHỮ ĐỌC NGANG, KHÔNG BỊ XOAY, NẰM GIỮA */
     .card-left {
         flex: 1; background: #d32f2f; color: #ffeb3b; 
         display: flex; align-items: center; justify-content: center;
@@ -77,7 +76,6 @@ st.markdown("""
 
     .stMarkdown p, label { color: white !important; font-size: 1.1rem !important; text-shadow: 1px 1px 3px black; }
     
-    /* Hoa và Lì xì rơi */
     .petal, .lixi { position: fixed; top: -10%; z-index: 9999; animation: fall linear infinite; font-size: 25px; pointer-events: none; }
     @keyframes fall { 0% { transform: translateY(0) rotate(0deg); } 100% { transform: translateY(110vh) rotate(360deg); } }
     
@@ -90,13 +88,6 @@ st.markdown("""
     <div class="petal" style="left:45%; animation-duration:10s;">🌸</div>
     <div class="lixi" style="left:65%; animation-duration:9s;">🧧</div>
     <div class="petal" style="left:85%; animation-duration:12s;">🌸</div>
-    <div class="lixi" style="left:65%; animation-duration:9s;">🧧</div>
-    <div class="petal" style="left:5%; animation-duration:8s;">🌸</div>
-    <div class="lixi" style="left:25%; animation-duration:11s;">🧧</div>
-    <div class="petal" style="left:45%; animation-duration:10s;">🌸</div>
-    <div class="lixi" style="left:65%; animation-duration:9s;">🧧</div>
-    <div class="petal" style="left:85%; animation-duration:12s;">🌸</div>
-    <div class="lixi" style="left:65%; animation-duration:9s;">🧧</div>
 """, unsafe_allow_html=True)
 
 # --- LOGIC ---
@@ -125,6 +116,7 @@ elif st.session_state.state == 'shaking':
     st.markdown('<div class="shaker">🏺</div>', unsafe_allow_html=True)
     time.sleep(2)
     
+    # ĐÃ FIX CÚ PHÁP TRONG DATA
     data = [
         {"gift": "1K", "wish": "Vạn Sự Như Ý", "img": "https://cdn-icons-png.flaticon.com/512/2614/2614741.png"},
         {"gift": "Bánh kẹp", "wish": "Gắn Kết Yêu Thương", "img": "https://cdn-icons-png.flaticon.com/512/3225/3225096.png"},
@@ -134,16 +126,16 @@ elif st.session_state.state == 'shaking':
         {"gift": "Bánh quy", "wish": "Ngọt Ngào Cả Năm", "img": "https://cdn-icons-png.flaticon.com/512/541/541732.png"},
         {"gift": "Bánh bông lan", "wish": "Mềm Mại Hạnh Phúc", "img": "https://cdn-icons-png.flaticon.com/512/2682/2682430.png"},
         {"gift": "5K", "wish": "Tấn Tài Tấn Lộc", "img": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png"},
-        {"gift": "Socola", "wish": "Tình Duyên Viên Mãn", "img": "https://media.istockphoto.com/id/1011038838/vi/vec-to/thanh-s%C3%B4-c%C3%B4-la-%C4%91%E1%BB%93-ng%E1%BB%8Dt-v%C3%A0-b%E1%BB%99-b%C3%A1nh-ng%E1%BB%8Dt-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-ph%C3%A1c-th%E1%BA%A3o-%C4%91%E1%BA%A7y.jpg?s=612x612&w=0&k=20&c=yiRIOOXDu5UsRleM6OH5NED5vEWERBOW_UlbHs9qeg8="},
+        {"gift": "Socola", "wish": "Tình Duyên Viên Mãn", "img": "https://cdn-icons-png.flaticon.com/512/2576/2576762.png"},
         {"gift": "1K", "wish": "Hạnh Phúc An Khang", "img": "https://cdn-icons-png.flaticon.com/512/2614/2614741.png"},
         {"gift": "Bánh kẹp", "wish": "Gắn Kết Yêu Thương", "img": "https://cdn-icons-png.flaticon.com/512/3225/3225096.png"},
         {"gift": "2K", "wish": "Mã Đáo Thành Công", "img": "https://cdn-icons-png.flaticon.com/512/2614/2614741.png"},
-        {"gift": "10K", "wish": "Phát tài phát lộc – Công thành danh toại", "img": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png"},
+        {"gift": "10K", "wish": "Phát tài phát lộc", "img": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png"},
         {"gift": "20K", "wish": "Phú Quý Cát Tường", "img": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png"},
-        {"gift": "Bánh quy", "wish": "NLộc tràn xuân mới – Niềm vui phơi phới", "img": "https://cdn-icons-png.flaticon.com/512/541/541732.png"},
-        {"gift": "Bánh bông lan", "wish": "Mềm Mại Hạnh Phúc", "https://cdn-media.sforum.vn/storage/app/media/wp-content/uploads/2023/10/cach-lam-banh-bong-lan-thumb.jpg"},
+        {"gift": "Bánh quy", "wish": "Niềm vui phơi phới", "img": "https://cdn-icons-png.flaticon.com/512/541/541732.png"},
+        {"gift": "Bánh bông lan", "wish": "Mềm Mại Hạnh Phúc", "img": "https://cdn-icons-png.flaticon.com/512/2682/2682430.png"},
         {"gift": "5K", "wish": "Tấn Tài Tấn Lộc", "img": "https://cdn-icons-png.flaticon.com/512/2489/2489756.png"},
-        {"gift": "Socola", "wish": "Ngàn lần như ý – Vạn sự như mơ", "img": "https://file.hstatic.net/200000331851/file/socola-ferrero-rocher_503b44453ec84763a9e22a957e069293_grande.jpg"},
+        {"gift": "Socola", "wish": "Vạn sự như mơ", "img": "https://cdn-icons-png.flaticon.com/512/2576/2576762.png"},
     ]
     result = random.choice(data)
     st.session_state.result = result
@@ -164,7 +156,6 @@ elif st.session_state.state == 'result':
         </div>
     """, unsafe_allow_html=True)
 
-    # KHAI BÁO CỘT Ở ĐÂY ĐỂ NÚT HIỆN RA
     c1, c2 = st.columns(2)
     with c1:
         if st.button("🧧 LẮC TIẾP", use_container_width=True):
